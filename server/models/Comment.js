07056267bf58ddacaccd971ce07ef55af0adaf05
Luke Yost <lukexyost@gmail.com>
2022-06-10 15:58:05 -0600

    comment shema done

--- server/models/Comment.js
+++ server/models/Comment.js
@@ -1,9 +1,20 @@
 import mongoose from "mongoose";
 const Schema = mongoose.Schema
-
+const ObjectId = Schema.Types.ObjectId
 
 
 export const CommentSchema = new Schema({
   name: {type: String, required: true},
-  description: {type: String, required: true}
+  description: {type: String, required: true},
+  postId: { type: ObjectId, ref: 'Post'},
+  creatorId: { type: ObjectId, ref: "Account"} 
+},
+{timestamps: true, toJSON: {virtuals: true}}
+)
+
+CommentSchema.virtual('creator' , {
+  localField: 'creatorId', 
+  foreignField: '_id',
+  justOne: true, 
+  ref: 'Profile'
 })
