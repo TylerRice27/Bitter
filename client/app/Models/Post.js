3ee5a3b5439f6a5c7fad16fdda7601a1b8e97993
Tyler <Tylerrayrice2@gmail.com>
2022-06-10 12:22:11 -0600

    Frontend Push 3

--- client/app/Models/Post.js
+++ client/app/Models/Post.js
@@ -1,8 +1,21 @@
 export class Post {
     constructor(data) {
+        this.id = data.id
         this.title = data.title
         this.name = data.name
         this.description = data.description
         this.imgUrl = data.imgUrl
+    }
+
+    get DataTemplate() {
+        return `
+        <div class="card text-start">
+            <div class="card-header text-center">${this.title}</div>
+            <div class="card-body">
+              <h4 class="card-title">${this.description}</h4>
+              <p class="card-text">${this.imgUrl}</p>
+            </div>
+          </div>
+        `
     }
 }
--- client/app/Services/PostsService.js
+++ client/app/Services/PostsService.js
@@ -1,12 +1,24 @@
-import { ProxyState } from "../AppState"
+import { ProxyState } from "../AppState.js"
+import { Post } from "../Models/Post.js"
+import { logger } from "../Utils/Logger.js"
+import { api } from "./AxiosService.js"
 
 
 
 
 class PostsService {
     async getPosts() {
-        let posts = ProxyState.posts
         const res = await api.get('api/posts')
+        logger.log('[Posts]', res.data)
+        const posts = res.data.map(p => new Post(p))
+        ProxyState.posts = posts
+    }
+
+
+    async createPosts(newPost) {
+        const res = await api.post('api/posts', newPost)
+        const post = new Post(res.data)
+        ProxyState.posts = [...ProxyState.posts, post]
     }
 
 }
